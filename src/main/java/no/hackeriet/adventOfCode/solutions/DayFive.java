4d27eef67b66240b43954a5ff12bb732f7920669
Alexander Kjäll <alexander.kjall@gmail.com>
2016-12-05 17:23:26 +0100

    rewrote day 5 part one as multithreaded

--- src/main/java/no/hackeriet/adventOfCode/solutions/DayFive.java
+++ src/main/java/no/hackeriet/adventOfCode/solutions/DayFive.java
@@ -4,29 +4,39 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 
 public class DayFive {
     public String hash(String input) throws NoSuchAlgorithmException {
 
-        MessageDigest md = MessageDigest.getInstance("MD5");
-
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < Integer.MAX_VALUE; i++) {
-            md.update((input + i).getBytes(StandardCharsets.ISO_8859_1));
+        Map<Integer, Character> map = new ConcurrentHashMap<>();
+        IntStream
+                .iterate(0, i -> i + 1)
+                .parallel()
+                .peek(i -> {
+                    try {
+                        MessageDigest md = MessageDigest.getInstance("MD5");
 
-            String s = String.format("%032x", new BigInteger(1, md.digest()));
+                        md.update((input + i).getBytes(StandardCharsets.ISO_8859_1));
 
-            if(s.startsWith("00000")) {
-                sb.append(s.charAt(5));
-            }
+                        String s = String.format("%032x", new BigInteger(1, md.digest()));
 
-            if(sb.length() == 8)
-                return sb.toString();
+                        if(s.startsWith("00000")) {
+                            map.put(i, s.charAt(5));
+                        }
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+                })
+                .allMatch(n -> map.size() < 8);
 
-            md.reset();
-        }
+        map.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey)).forEach(e -> sb.append(e.getValue()));
 
-        return "";
+        return sb.toString();
     }
 }
