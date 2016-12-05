82d41703db29ebf52330cd4523f322db51c66c74
Alexander Kjäll <alexander.kjall@gmail.com>
2016-12-05 07:31:23 +0100

    fixed test

--- src/test/java/no/hackeriet/adventOfCode/DayTwoTest.java
+++ src/test/java/no/hackeriet/adventOfCode/DayTwoTest.java
@@ -3,10 +3,12 @@ package no.hackeriet.adventOfCode;
 import no.hackeriet.adventOfCode.solutions.DayTwo;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class DayTwoTest {
     @Test
     public void press() throws Exception {
-        new DayTwo().press(Util.read("dayTwoExample"));
+        assertEquals("[1, 9, 8, 5]", new DayTwo().press(Util.read("dayTwoExample")));
     }
 
 }
